3e3e0b43e04388033aa0fa9399a457e72ce58260
jianzhang.zou <jianzhang.zou@alibaba-inc.com>
2017-01-09 00:12:06 +0800

    change init-config with purcell's style; include benchmark by purcell

---
+++ .emacs.d/init.el
@@ -0,0 +1,75 @@
+;;- ------------------------------
+;; bootstrap config
+;;- ------------------------------
+;; load site-lisp default path
+(add-to-list 'load-path "~/.emacs.d/lisp")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/packages")
+
+(require 'init-benchmarking) ;; Measure startup time
+(require 'init-local)
+
+;; Color Theme
+(add-to-list 'load-path "~/.emacs.d/site-lisp/color-theme-6.6.0")
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-dark-blue2)
+
+;; multi-term
+(require 'multi-term)
+(setq multi-term-program "/bin/bash")
+
+;; smex
+(require 'smex)
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; gtags
+;;start Emacs and execute gtags-mode function.
+(autoload 'gtags-mode "gtags" "" t)
+(add-hook 'c-mode-common-hook
+	  (lambda ()
+	    (require 'gtags)
+	    (gtags-mode t)
+	    (when (not (string-match "/usr/src/linux/" (expand-file-name default-directory)))  
+	      (djcb-gtags-create-or-update))))
+
+;; eim
+(add-to-list 'load-path "~/.emacs.d/site-lisp/eim")
+(autoload 'eim-use-package "eim" "Another emacs input method")
+(register-input-method
+ "eim-py" "euc-cn" 'eim-use-package
+ "eim拼音" "eim拼音" "py.txt")
+;; ";" to exchange englise input
+(require 'eim-extra)
+(global-set-key ";" 'eim-insert-ascii)
+
+;; stardict
+(require 'stardict)
+(global-set-key (kbd "<M-f3>") 'view-stardict-in-buffer)
+
+;; window-number
+(require 'window-number)  
+(window-number-mode 1)  
+(window-number-meta-mode 1)  
+
+;; magit
+(add-to-list 'load-path "~/.emacs.d/site-lisp/dash")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/with-editor")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/magit/lisp")
+(require 'magit)
+;; magit document
+(with-eval-after-load 'info
+  (info-initialize)
+  (add-to-list 'Info-directory-list
+	       "~/.emacs.d/site-lisp/magit/Documentation/"))
+(put 'upcase-region 'disabled nil)
+
+;; neotree
+(add-to-list 'load-path "~/.emacs.d/site-lisp/neotree")
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+
+
+(provide 'init)
---
+++ .emacs.d/lisp/init-local.el
@@ -0,0 +1,110 @@
+;; load site-lisp default path
+(add-to-list 'load-path "~/.emacs.d/site-lisp")
+
+;; encoding
+;; disable CJK coding/encoding (Chinese/Japanese/Korean characters)
+(setq utf-translate-cjk-mode nil) 
+(set-language-environment 'utf-8)
+(setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(unless (eq system-type 'windows-nt)
+  (set-selection-coding-system 'utf-8))
+(prefer-coding-system 'utf-8)
+
+;; englisg font
+(set-frame-font "monospace 10")
+
+;; display line number & column number
+(setq column-number-mode t)
+(setq line-number-mode t)
+
+;; kick off menubar & toolbar
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+;; set date\time display
+(setq display-time-day-and-date t)
+(display-time)
+
+;; you know
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; display image
+(auto-image-file-mode t) 
+
+;; automatic load abbreviations tables
+(setq-default abbrev-mode t)
+(read-abbrev-file "~/.abbrev_defs")
+(setq save-abbrevs t)
+
+;; In every buffer, the line which contains the cursor 
+;; will be fully highlighted
+(global-hl-line-mode 1)
+
+;; use clipboard
+(setq x-select-enable-clipboard t)
+
+;; use clipboard
+(setq x-select-enable-clipboard t)
+
+(setq visible-bell t)
+(setq  scroll-margin 2
+       scroll-conservatively 10000)
+(setq kill-ring-max 200)
+(setq transient-mark-mode t)
+
+;; matching pairs of parentheses
+(show-paren-mode 1)
+
+;; all backups goto ~/.backups instead in the current directory
+(setq backup-directory-alist (quote (("." . "~/.backups"))))
+
+;; Meta-g: goto line
+(global-set-key (kbd "M-g") 'goto-line)
+
+;; open recent file
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 100)
+(global-set-key (kbd "C-c C-f") 'recentf-open-files)
+
+;; tramp
+(require 'tramp)
+
+;; indent
+(setq-default indent-tabs-mode nil)
+(setq default-tab-width 4)
+(setq-default c-basic-offset 4)
+(setq c-default-style "linux"
+      c-basic-offset 4)
+(c-set-offset 'substatement-open 0)
+
+;; display function name
+(which-function-mode t)
+
+;; fold/toggle
+(load-library "hideshow")
+(add-hook 'c-mode-common-hook 'hs-minor-mode)
+(add-hook 'objc-mode-hook 'hs-minor-mode)
+(add-hook 'java-mode-hook 'hs-minor-mode)
+(add-hook 'emacs-lisp-mode-hook 'hs-minor-mode)
+(add-hook 'lisp-mode-hook 'hs-minor-mode)
+(add-hook 'scheme-mode-hook 'hs-minor-mode)
+(global-set-key (kbd "C-,") 'hs-toggle-hiding)   
+(global-set-key (kbd "C-.") 'hs-show-all)        
+(global-set-key (kbd "C-/") 'hs-hide-all)        
+
+;; display weekly calender
+(copy-face font-lock-constant-face 'calendar-iso-week-face)
+(set-face-attribute 'calendar-iso-week-face nil
+                      :height 0.5 :foreground "salmon")
+(setq calendar-intermonth-text
+      '(propertize
+        (format "W%02d"
+                (car
+                 (calendar-iso-from-absolute
+                  (calendar-absolute-from-gregorian (list month day year)))))
+        'font-lock-face 'calendar-iso-week-face))
+
+(provide 'init-local)
